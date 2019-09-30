b8c9777d094a4484fec2b55a38e285b840e09218
cedricmugabe <54921896+cedricmugabe@users.noreply.github.com>
2019-09-29 20:36:26 -0700

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,40 +1,40 @@
-
 import os
 import csv
-filepath = os.path.join( '..','Resources','election_data.csv')
-totalcount = 0; kcount = 0; ccount = 0; lcount = 0; ocount = 0; max_votecount = 0
-def percentage (part, whole):
-    return 100 * float(part)/float(whole)
-with open(filepath, newline='') as csvfile:
-     csvreader = csv.reader(csvfile, delimiter=',')
-    for i in csvreader:
-         voterid = i[0]
-         country = i[1]
-         candidate = i[2]
-         
-         totalcount = totalcount + 1
-         if candidate =="Khan":
-            kcount = kcount + 1
-         if candidate =="Correy":
-            ccount = ccount + 1
-         if candidate =="Li":
-            lcount = lcount + 1
-         if candidate =="O'Tooley":
-            ocount = ocount + 1
-    candidatevote = {"Khan": kcount,"Correy": ccount,"Li" :lcount, "O'Tooley": ocount}
-    for candidate, value in candidatevote.items():
-         if value > max_votecount:
-            max_votecount = value
-            winner = candidate
-print(f'Election Results'+'\n')
-print(f'-------------------------------'+'\n')
-print(f'Total Votes: {totalcount}'+'\n')
-print(f'-------------------------------'+'\n')
-print(f'Khan: {percentage(kcount,totalcount):.3f}%  ({kcount})')
-print(f'Correy: {percentage(ccount,totalcount):.3f}%  ({ccount})')
-print(f'Li: {percentage(lcount,totalcount):.3f}%  ({lcount})')
-print(f'O\'Tooley: {percentage(ocount,totalcount):.3f}%  ({ocount})')
-print(f'--------------------------------'+'\n')
-print(f'Winner: {winner} '+'\n')
-print(f'--------------------------------'+'\n')
 
+budget_data.csv = os.path.join('..', 'Resources', 'budget_data.csv')
+
+mcount = 0; total = 0; PreValue = 0; Diff = 0; DiffMax = 0; DiffMin = 0
+
+
+with open(budget_data.csv, newline='') as csvfile:
+     csvreader = csv.reader(csvfile, delimiter=',')
+     csv_header = next(csvreader)
+     print(f'Financial Analysis'+'\n')
+     print(f'----------------------------'+'\n')
+     for i in csvreader:
+         month = i[0]
+         Amount = i[1]
+         iAmount = int(Amount)
+         Diff =  iAmount - PreValue
+        
+         if DiffMax < Diff:
+            DiffMax = Diff
+            DiffMaxDate = month
+         
+         if DiffMin > Diff:
+            DiffMin = Diff
+            DiffMinDate = month
+
+         PreValue = iAmount   
+       
+         mcount = mcount + 1
+         total += int(Amount) 
+
+
+print(f'Total Months : {mcount}')
+
+print(f'Total: $ {total}')
+
+print(f'Greatest Increase in Profits: {DiffMaxDate} : ($ {DiffMax})')
+
+print(f'Greatest Decrease in Profits: {DiffMinDate} : ($ {DiffMin})')
